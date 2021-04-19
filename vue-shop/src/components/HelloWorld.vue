460bd86837c10f690c81d590cba21619d8273ac2
Sergio GQ <sergio.gonzalez@symphony.com>
2021-04-19 23:43:44 +0200

    Video 2

---
+++ vue-shop/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{product}}</h1>
+</template>
+
+<script lang="ts">
+  import { ref, defineComponent } from 'vue'
+
+  export default defineComponent({
+    data() {
+      return {
+        product: 'Socks'
+      }
+    }
+  })
+</script>
+
+  <style scoped>
+  a {
+    color: #42b983;
+  }
+
+  label {
+    margin: 0 0.5em;
+    font-weight: bold;
+  }
+
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
+</style>
