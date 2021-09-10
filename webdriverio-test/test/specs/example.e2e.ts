8829aff038277afad59d360f049504ca59a1a2a7
Sergio GQ <sergio.gonzalez@symphony.com>
2021-09-11 01:04:03 +0200

    Used aatt tool

--- webdriverio-test/test/specs/example.e2e.ts
+++ webdriverio-test/test/specs/example.e2e.ts
@@ -1,10 +1,11 @@
 let assert = require('assert');
 const AxeBuilder = require('@axe-core/webdriverio').default;
 import { assertAccessible } from '@sa11y/wdio';
+const { evaluate } = require('aatt');
 
-describe('my awesome website', () => {
+describe('accessibility testing to WebdriverIO', () => {
 
-    it('should do some assertions', async () => {
+    it('test with @axe-core/webdriverio', async () => {
 
         await browser.url('https://webdriver.io');
         await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
@@ -13,7 +14,7 @@ describe('my awesome website', () => {
 
         // Source: https://www.npmjs.com/package/@axe-core/webdriverio
         // Example: https://github.com/dequelabs/axe-core-npm/blob/develop/packages/webdriverio/src/test.ts
-        const axe = new AxeBuilder({ client: browser }).withTags(['wcag2a', 'wcag2aa']);
+        const axe = new AxeBuilder({ client: browser }).withTags(['wcag2a', 'wcag2aa']).disableRules('color-contrast');
         const results = await axe.analyze();
 
         // assert there are no violations
@@ -21,13 +22,41 @@ describe('my awesome website', () => {
         console.log(results.violations, 'vol')
         console.log(results.violations.length, 'len')
         assert.equal(results.violations.length, 0, 'Expected no a11y violations');
+    });
+
+    it('test with @sa11y/wdio', async () => {
+        await browser.url('https://webdriver.io');
+        await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
+        const title = await browser.getTitle();
+        assert.equal(title, 'WebdriverIO · Next-gen browser and mobile automation test framework for Node.js | WebdriverIO');
 
         // Source: https://www.npmjs.com/package/@sa11y/wdio
         // Exception list is a map of rule to corresponding css targets that needs to be filtered from a11y results
         const exceptions = {
-            'color-contrast': ['.button.button--outline[href$="gettingstarted"]'],
+            'link-name': ['.frameworkLogos_lURf[href$="reactjs\\.org\\/"]'],
         };
+        const rules: any = { runOnly: { type: 'tag', values: ['wcag2a'] }};
         // Check accessibility of a particular element and filtering out results from given exception list
-        await assertAccessible({ scope: browser.$('.main-wrapper'), exceptionList: exceptions });
+        await assertAccessible({ scope: browser.$('.main-wrapper'), exceptionList: exceptions, rules: rules });
+    });
+
+    it('test with aatt', async () => {
+        await browser.url('https://webdriver.io');
+
+        const currentHTML = await $('body').getHTML();
+
+        // Source: https://github.com/paypal/AATT
+        const results: any = await new Promise(resolve => {
+            evaluate({
+                source: currentHTML,
+                output: "json",
+                engine: "axe",
+                level: "WCAG2A"
+            }).then((result: string) => {
+                console.log('Results: ', JSON.parse(result));
+                resolve(JSON.parse(result));
+            });
+        });
+        assert.equal(results.filter((result: any) => result.impact == 'serious').length, 0, 'Expected no serious violations');
     });
 });
